daefaca185e584acc605f9962a8b358baa551c54
xiaoxu.gao <highsmallxu@gmail.com>
2019-05-27 22:47:51 +0200

    implement voeg en nieuw woord toe

--- Vuejs/src/App.vue
+++ Vuejs/src/App.vue
@@ -14,7 +14,6 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="teal" dark fixed app>
-      <!-- style="padding-left:200px !important" -->
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Je privaat woordenboek</v-toolbar-title>
     </v-toolbar>
@@ -22,7 +21,7 @@
       <router-view></router-view>
     </v-content>
     <v-footer color="teal" app inset>
-      <v-btn absolute dark fab buttom right color="pink" style="bottom:50px">
+      <v-btn absolute dark fab buttom right color="pink" style="bottom:50px" @click="add_word">
         <v-icon>add</v-icon>
       </v-btn>
       <v-layout justify-center row wrap>
@@ -32,6 +31,41 @@
         </v-flex>
       </v-layout>
     </v-footer>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-form ref="form">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Voeg een nieuw woord toe</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6>
+                  <v-text-field v-model="form.nederlands" label="Nederlands" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm12>
+                  <v-text-field v-model="form.voorbeeld" label="Voorbeeld" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm4>
+                  <v-text-field label="ik"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm4>
+                  <v-text-field label="jij"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm4>
+                  <v-text-field label="wij"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="add_word_confirm">Voorleggen</v-btn>
+            <v-btn flat color="primary" @click="add_word_cancel">Annuleren</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -39,9 +73,32 @@
 export default {
   name: "App",
   data() {
+    const defaultForm = Object.freeze({
+      nederlands: "",
+      voorbeeld: ""
+    });
     return {
-      drawer: null
+      form: Object.assign({}, defaultForm),
+      drawer: null,
+      dialog: false
     };
+  },
+  methods: {
+    add_word(evt) {
+      console.log("invoke add word function");
+      this.dialog = true;
+    },
+    add_word_confirm(evt) {
+      console.log("is going to send POST api");
+      console.log(evt);
+      console.log(this.form.nederlands);
+      //TODO: send POST api
+      this.dialog = false;
+    },
+    add_word_cancel(env) {
+      console.log("cancel");
+      this.dialog = false;
+    }
   }
 };
 </script>
